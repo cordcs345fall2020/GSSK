1d8350f3cc3e28e012512a3b99ff394422053ef8
Shristi321 <59893870+Shristi321@users.noreply.github.com>
2020-11-27 13:07:37 -0600

    Update app_start.php

--- main_app/chat/functions/app_start.php
+++ main_app/chat/functions/app_start.php
@@ -5,7 +5,7 @@ session_start();
 ob_start();
 
 // Define consts
-define("URL", "http://127.0.0.1:8000/");
+define("URL", "");
 define("SITE", "Chat Application");
 
 // Require all files
@@ -15,6 +15,7 @@ require_once('chat.php');
 require_once('friend.php');
 require_once('helper.php');
 require_once('encrypt.php');
+
 
 $key = "vR5sH6gZ7sK6bD6m";
 
