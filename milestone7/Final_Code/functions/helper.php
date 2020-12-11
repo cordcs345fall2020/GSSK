c86e65d49d71332c1981f5fc94a4a2f650cfc9f2
Khan <33772323+Gansaikhan@users.noreply.github.com>
2020-12-11 16:21:17 -0600

    removal

--- milestone7/Final_Code/functions/helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function redirect($page, $status = 303){
-    if((empty($page))){
-        $page = "index";
-    }
-    header("Location: " . URL . $page . '.php', true, $status);
-    die();
-}
